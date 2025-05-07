96e70ccf3369dccaa3e44b32f60de4328be97faa
miguelavanci <miguelavanci825@gmail.com>
2025-05-07 17:51:50 +0000

    aula 07/05

--- index.js
+++ index.js
@@ -9,8 +9,16 @@ app.use(express.static('public'));
 
 app.get('/', (request, response) => {
     //response.send('Testando!!!');
-    response.render('home');
+    response.render('home', {nome : "miguel"});
 });
+
+app.get ('/tabuada', (request, response) => {
+    let resultado = []
+    for(let cont = 1;cont <= 10;cont++){
+        resultado.push(cont *5)
+    }
+    response.render('tabuada',{valores : resultado})
+})
 
 app.listen(3000, () => {
     console.log('Servidor na porta 3000');
